finally fixed the rotation bug

analyse_numpy.py
@@ -12,7 +12,8 @@ args = parser.parse_args()
 image_names = args.image[0]
 
 for image_name in image_names.split(" "):
-    test_dir = "/usr/project/xtmp/mammo/binary_Feb/binary_context_roi/binary_train_spiculated_augmented/spiculated/" + image_name
+    test_dir = "/usr/project/xtmp/mammo/binary_Feb/binary_context_roi/test_spiculated_temp/spiculated/" + image_name
+    print(test_dir)
     arr = np.load(test_dir)
     print(arr.shape)
     print("Non zero count is " , np.count_nonzero(arr))
@@ -27,9 +28,3 @@ for image_name in image_names.split(" "):
     plt.close()
 
 
-# DP_AAHR_R_CC_1#0#3.npy
-#DP_AAYY_R_MLO_5#0#4.npy
-#DP_ABAC_L_MLO_3#1#0.npy
-# DP_ABBQ_L_MLO_3#1#3.npy
-#DP_ACYS_L_CC_2#0#0.npy
-# DP_AGCW_R_CC_5#0#0.npy   DP_AIDR_L_MLO_3#0#3.npy   DP_AJZL_L_MLO_3#0#3.npy

vanilla_network.py
@@ -50,19 +50,13 @@ testloader = torch.utils.data.DataLoader(
 device = torch.device("cuda" if torch.cuda.is_available()
                                   else "cpu")
 
-model = models.vgg16(pretrained=True)
+model = models.resnet34(pretrained=True)
 for param in model.parameters():
     param.requires_grad = False
 
-model.fc = nn.Sequential(nn.Linear(512 * 7 * 7, 4096),
-                         nn.ReLU(True),
-                        nn.Dropout(),
-                        nn.Linear(4096, 4096),
-                        nn.ReLU(True),
-                        nn.Dropout(),
-                        nn.Linear(4096, 2),)
+model.fc = nn.Sequential(nn.Linear(512,2),)
 
-criterion = nn.NLLLoss()
+criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.fc.parameters(), lr=1e-3)
 
 model.to(device)