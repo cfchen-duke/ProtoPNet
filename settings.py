544bbfb0711f32bd0efbdd5c1722246a60e52522
si-lab <gianlucarloni8@gmail.com>
2022-03-22 16:08:31 +0100

    customizzazione del nome dell'esperimento (ora con la data)

--- settings.py
+++ settings.py
@@ -6,19 +6,19 @@ base_architecture = 'resnet50'
 img_size = 124# 224 #336
 num_classes = 2 
 num_prots_per_class = 10
-prototype_shape = (num_classes*num_prots_per_class, 256, 1, 1) #40 #60 #16 #40
+num_filters = 256
+prototype_shape = (num_classes*num_prots_per_class, num_filters, 1, 1) #40 #60 #16 #40
 prototype_activation_function = 'log'
 add_on_layers_type = 'regular'
 
-experiment_run = '1_ADNI_124IMGSIZE_10PROTPERCLASS_256FILTERS'
 
 data_path = './datasets/' #
 train_dir = data_path + 'push_augmented/' #
 test_dir = data_path + 'valid_augmented' #'valid/' #
 train_push_dir = data_path + 'push/' #
-train_batch_size = 20 #30 #25 #12 #20 #
-test_batch_size = 20 #25 #20 #10 #
-train_push_batch_size = 20 #30 #25 #15 #20 # 10 #
+train_batch_size = 80 #20 #30 #25 #12 #20 #
+test_batch_size = 100 #20 #25 #20 #10 #
+train_push_batch_size = 75 #20 #30 #25 #15 #20 # 10 #
 
 joint_optimizer_lrs = {'features': 1e-4,
                        'add_on_layers': 3e-3,
@@ -42,3 +42,6 @@ num_warm_epochs = 5
 
 push_start = 10
 push_epochs = [i for i in range(num_train_epochs) if i % 10 == 0]
+
+from time import gmtime,strftime
+experiment_run = f'ADNI_{strftime("%a_%d_%b_%Y_%H:%M:%S", gmtime())}'
