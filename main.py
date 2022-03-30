6fc27b6ea547bb26bbe6b66be56caafb59a9cbcd
si-lab <gianlucarloni8@gmail.com>
2022-03-30 12:11:24 +0200

    inserito commento sul DistributedDataParallel

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
     parser.add_argument('-gpuid', nargs=1, type=str, default='0') #TODO
     # python3 main.py -gpuid=0,1,2,3
     args = parser.parse_args()
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpuid[0]
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpuid[0] #TODO
     print(os.environ['CUDA_VISIBLE_DEVICES'])
     
     # book keeping namings and code
@@ -131,7 +131,7 @@ def main():
     #if prototype_activation_function == 'linear':
     #    ppnet.set_last_layer_incorrect_connection(incorrect_strength=0)
     ppnet = ppnet.cuda()
-    ppnet_multi = torch.nn.DataParallel(ppnet)
+    ppnet_multi = torch.nn.DataParallel(ppnet) #TODO usare invece DistributedDataParallel!
     class_specific = True
     
     # define optimizer
