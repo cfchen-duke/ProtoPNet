422bf386b7b03321c2b29dfa7dd08af172a2bfd2
gianlucarloni <gianlucarloni8@gmail.com>
2022-05-09 17:13:47 +0200

    Set LRates to 1e-06, set batch_size=40 for train/push and =2 for test, num_features=512

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 experiment_task = 'CBIS_massBenignMalignant' #'ADNI' 
 # base_architecture = 'vgg19'
-base_architecture = 'resnet50'
+base_architecture = 'resnet18' #'resnet50'
 #base_architecture = 'densenet121'
 
 
 img_size = 224 #124# 224 #336
 num_classes = 2 
 num_prots_per_class = 10
-num_filters = 256 #128 #256
+num_filters = 512 #256 #128 #256
 prototype_shape = (num_classes*num_prots_per_class, num_filters, 1, 1) #40 #60 #16 #40
 prototype_activation_function = 'log'
 add_on_layers_type = 'regular'
@@ -23,19 +23,19 @@ test_dir = data_path + 'valid' #'valid/' #
 # test_dir = data_path + 'test/' #'valid/' #TODO
 
 train_push_dir = data_path + 'push/' #
-train_batch_size = 25 #50 #35 #80 #20 #30 #25 #12 #20 #
-test_batch_size = 20 #60 #40 #100 #20 #25 #20 #10 #
-train_push_batch_size = 25 #50 #35 #75 #20 #30 #25 #15 #20 # 10 #
+train_batch_size = 40 #25 #50 #35 #80 #20 #30 #25 #12 #20 #
+test_batch_size = 2 #20 #60 #40 #100 #20 #25 #20 #10 #
+train_push_batch_size = 40 #25 #50 #35 #75 #20 #30 #25 #15 #20 # 10 #
 
-joint_optimizer_lrs = {'features': 1e-6,#1e-4 #TODO
-                       'add_on_layers': 3e-3,
-                       'prototype_vectors': 3e-3}
+joint_optimizer_lrs = {'features': 1e-06,#1e-4 #TODO
+                       'add_on_layers': 1e-06, #3e-3,
+                       'prototype_vectors': 1e-06} #3e-3}
 joint_lr_step_size = 10 #5 #TODO
 
-warm_optimizer_lrs = {'add_on_layers': 3e-3,
-                      'prototype_vectors': 3e-3}
+warm_optimizer_lrs = {'add_on_layers': 1e-06, #3e-3,
+                      'prototype_vectors': 1e-06} #3e-3}
 
-last_layer_optimizer_lr = 1e-4
+last_layer_optimizer_lr = 1e-06 #1e-4
 
 coefs = {
     'crs_ent': 1,
@@ -44,7 +44,7 @@ coefs = {
     'l1': 1e-4
 }
 
-num_train_epochs = 200 #TODO
+num_train_epochs = 1000 #TODO
 num_warm_epochs = 5
 
 push_start = 10
